626b894096099b4066f53e015633f28d2eebeece
ciekals11 <ciekals11@gmail.com>
2020-12-05 17:26:55 +0100

    FEAT - add issue update node

---
+++ nodes/issue-update.js
@@ -0,0 +1,26 @@
+module.exports = function(RED) {
+    function IssueUpdateNode(config) {
+        RED.nodes.createNode(this, config);
+        let { Youtrack } = require('youtrack-rest-client');
+
+        this.server = RED.nodes.getNode(config.server);
+        this.youtrack = new Youtrack({
+            baseUrl: this.server.host,
+            token: this.server.token
+        });
+
+        this.on('input', (msg) => {
+            if (!msg.hasOwnProperty('payload')) {
+                this.state({ color: 'red', msg: 'Don\' payload send!' });
+                return;
+            }
+
+            this.youtrack.issues.update(msg.payload).then(issue => {
+                msg.issue = {issue};
+                this.send(msg);
+            });
+        });
+    }
+
+    RED.nodes.registerType("issue-update", IssueUpdateNode);
+};
